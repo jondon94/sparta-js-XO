36773c253ce16699879f4e08f84f14b4dab3f357
Tech-A66 <tech-a66@Tech-A66s-MacBook-Air.local>
2017-11-02 16:10:52 +0000

    getting started i have the pseudocode

---
+++ main.js
@@ -0,0 +1,15 @@
+// $(function(){
+//   $('#button').click(function(){
+//     console.log('ive been clicked');
+//   })
+//
+//   $('#button').on('click', function(){
+//     console.log('ivebeen clicked again');
+//   })
+//
+// })
+
+//have 2 diffferent placable tiles X and O
+//be able to alternate which tile is placed in each square (ONLY 1)
+//Generate a winning condition that stops the game
+//have a button that resets the board.
