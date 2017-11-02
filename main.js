f39c6c0138c53f9ab8f6e7c3cd5690f3dbc79ab9
Tech-A66 <tech-a66@Tech-A66s-Air.home>
2017-11-02 21:50:43 +0000

    reset button works, unable to create alternating turns still

--- main.js
+++ main.js
@@ -1,38 +1,59 @@
+//////////////////////////////////////Variables
+
 var player1 = true;
 var player2 = false;
+// var connect3 = (["", "", ""] || ["", "", ""] || ["", "", ""]);
 
-function playTurnX(){
+//////////////////////////////////////Game Controller
+
+function changeTurn(){
+
+  var playTurnX = player1
+  var playTurnO = player2
+
   if (player1 === true){
-    $(function(){
-      $('td').on('click', function(){
-        $(this).addClass('X').append("X")
-        console.log('ivebeen clicked by x');
+      player2 = false;
+      $(function playTurnX(){
         player2 = true;
         player1 = false;
+        $('td').on('click', function(){
+        $(this).addClass('X').append("X")
+        console.log('ivebeen clicked by x');
+        })
+      })
+  } else (player2 === true)
+      player1 = false;
+      $(function playTurnO(){
+        player1 = true;
+        player2 = false;
+        $('td').on('click', function(){
+        $(this).addClass('O').append("O")
+        console.log('ive been clicked by o');
     })
   })
 }
 
-
-function playTurnO(){
-  (player2 === true)
-  $(function(){
-    $('td').on('click', function(){
-      $(this).addClass('O').append("O")
-      console.log('ivebeen clicked by o');
-      player1 = true;
-      player2 = false;
-    })
+//////////////////////////////////////Clickable reset
+$(function(){
+  $('#reset').click(function(){
+    $('td').html($(""))
+    console.log('ive been clicked');
   })
-}
+})
 
-// function changeTurn(){
-//   playTurnX()
-//   playTurnO()
+//////////////////////////////////////JUNK
+
+// function Winner(){
+//   if (connect3 === false) {
+//     changeTurn()
 //
+//
+//   }else{
+//
+//   }
 // }
 
-
+changeTurn()
 
 // //////X Clicker
 // $(function(){
@@ -50,7 +71,7 @@ function playTurnO(){
 //   })
 // })
 
-<!-- Working CLickable table-->
+<!-- Working CLickable reset-->
 // $(function(){
 //   $('table').on('click', function(){
 //     console.log('ivebeen clicked again');
@@ -63,11 +84,3 @@ function playTurnO(){
 //be able to alternate which tile is placed in each square (ONLY 1)
 //Generate a winning condition that stops the game
 //have a button that resets the board.
-
-
-// $(function(){
-//   $('#reset').click(function(){
-//     console.log('ive been clicked');
-//   })
-// })
-///////Clickable reset
